cc34522e18c965b8ad226237b4f04562e330d701
Dhakshayini Usha R <dhakshayiniusha@gmail.com>
2025-07-21 20:31:16 +0530

    Update Static_StorageClass.cpp

--- Static_StorageClass.cpp
+++ Static_StorageClass.cpp
@@ -22,3 +22,20 @@ int main(){
     cout<<"Address of global variable a: "<<&a;
     return 0;
 }
+
+/* Sample Output
+Address of func variable a : 0x404194
+Size of variable           : 4
+The variable is            : 1
+
+Address of func variable a : 0x404194
+Size of variable           : 4
+The variable is            : 2
+
+Address of func variable a : 0x404194
+Size of variable           : 4
+The variable is            : 3
+
+The variable is            : 5
+Address of global variable a : 0x404040
+    */
